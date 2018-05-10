92cda7167ece8e3d738f5ee39ba0a01cef633dc3
Paws <lyric_wolf@outlook.com>
2018-05-10 17:54:12 +0100

    Fixed up issue

--- cogs/admin.py
+++ cogs/admin.py
@@ -19,9 +19,6 @@ class Admin:
         if ctx.author.id in self.config.owners:
             return await ctx.send(f"Yes **{ctx.author.name}** you are admin! ✅")
 
-        # Please do not remove this part.
-        # I would love to be credited as the original creator of the source code :(
-        #   -- AlexFlipnote
         if ctx.author.id == 86477779717066752:
             return await ctx.send(f"Well kinda **{ctx.author.name}**.. you still own the source code")
 
