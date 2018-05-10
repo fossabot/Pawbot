14e568d9bd06acbd78205f8795e3d20f32fdf848
Paws <lyric_wolf@outlook.com>
2018-05-10 18:05:05 +0100

    Modified info

--- cogs/info.py
+++ cogs/info.py
@@ -54,7 +54,7 @@ class Information:
     @commands.command(aliases=['supportserver', 'feedbackserver'])
     async def botserver(self, ctx):
         """ Get an invite to our support server! """
-        if isinstance(ctx.channel, discord.DMChannel) or ctx.guild.id != 86484642730885120:
+        if isinstance(ctx.channel, discord.DMChannel) or ctx.guild.id != 353684556848562176:
             return await ctx.send(f"**Here you go {ctx.author.name} 🍻\n<{repo.invite}>**")
 
         await ctx.send(f"**{ctx.author.name}** this is my home you know :3")
@@ -67,13 +67,13 @@ class Information:
         embed = discord.Embed(colour=ctx.me.top_role.colour)
         embed.set_thumbnail(url=ctx.bot.user.avatar_url)
         embed.add_field(name="Uptime", value=self.get_bot_uptime(), inline=False)
-        embed.add_field(name="Developer", value="AlexFlipnote#0001", inline=True)
+        embed.add_field(name="Developer", value="Paws#0001", inline=True)
         embed.add_field(name="Library", value="discord.py", inline=True)
         embed.add_field(name="Commands loaded", value=len([x.name for x in self.bot.commands]), inline=True)
         embed.add_field(name="Servers", value=len(ctx.bot.guilds), inline=True)
         embed.add_field(name="RAM", value=f"{ramUsage:.2f} MB", inline=True)
 
-        await ctx.send(content=f"ℹ About **{ctx.bot.user}** | **{repo.version}**", embed=embed)
+        await ctx.send(content=f"ℹ About **{ctx.bot.user.name}** | **{repo.version}**", embed=embed)
 
 
 def setup(bot):
--- utils/repo.py
+++ utils/repo.py
@@ -1,7 +1,7 @@
 from utils import default
 
 version = "v1.2.4"
-invite = "https://discord.gg/DpxkY3x"
+invite = "https://discord.gg/j8g4CHW"
 owners = default.get("config.json").owners
 
 
