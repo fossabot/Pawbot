1b82804e7080eebade43aea0a870c80ef852a845
Paws <lyric_wolf@outlook.com>
2018-11-10 19:13:34 +0000

    uwu v2

--- data.py
+++ data.py
@@ -11,7 +11,7 @@ class Bot(AutoShardedBot):
         self.db = kwargs.pop("db")
 
     async def on_message(self, msg):
-        if not self.is_ready() or msg.author.bot or not permissions.can_send(msg):
+        if not self.is_ready() or msg.author.bot or not permissions.can_send(msg) or msg.author.id in config.userBlacklist or msg.guild.id in config.guildBlacklist:
             return
         else:
             await self.process_commands(msg)
