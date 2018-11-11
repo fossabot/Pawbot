b09fe34bae2eeeefc9ca29f96317ff66f2192182
Paws <lyric_wolf@outlook.com>
2018-11-11 11:55:47 +0000

    Hacky af blacklist check for dms and guilds

--- data.py
+++ data.py
@@ -11,7 +11,13 @@ class Bot(AutoShardedBot):
         self.db = kwargs.pop("db")
 
     async def on_message(self, msg):
-        if not self.is_ready() or msg.author.bot or not permissions.can_send(msg) or msg.author.id in config.userBlacklist or msg.guild.id in config.guildBlacklist:
-            return
+        if msg.guild is None:
+            if not self.is_ready() or msg.author.bot or not permissions.can_send(msg) or msg.author.id in config.userBlacklist:
+                return
+            else:
+                await self.process_commands(msg)
         else:
-            await self.process_commands(msg)
+            if not self.is_ready() or msg.author.bot or not permissions.can_send(msg) or msg.author.id in config.userBlacklist or msg.guild.id in config.guildBlacklist:
+                return
+            else:
+                await self.process_commands(msg)
