7b387b5c4e1087c2d1328b80bae68e836ad923dd
Paws <lyric_wolf@outlook.com>
2018-11-11 13:54:03 +0000

    Suggestions and making stuff nicer

--- cogs/info.py
+++ cogs/info.py
@@ -4,6 +4,7 @@ import psutil
 import os
 import asyncio
 
+from discord_webhook import DiscordWebhook, DiscordEmbed
 from discord.ext import commands
 from datetime import datetime
 from utils import repo, default
@@ -38,9 +39,9 @@ class Information:
     async def ping(self, ctx):
         """ Pong! """
         before = time.monotonic()
-        message = await ctx.send("Pong")
+        message = await ctx.send("Did you just ping me?!")
         ping = (time.monotonic() - before) * 1000
-        await message.edit(content=f"Pong   |   {int(ping)}ms")
+        await message.edit(content=f"`MSG :: {int(ping)}ms\nAPI :: {round(self.bot.latency * 1000)}ms`")
 
     @commands.command(aliases=['joinme', 'join', 'botinvite'])
     async def invite(self, ctx):
@@ -65,7 +66,7 @@ class Information:
         embed = discord.Embed(title=f"About **{ctx.bot.user.name}** | **{repo.version}**", colour=ctx.me.colour, url="https://discordapp.com/oauth2/authorize?client_id=460383314973556756&scope=bot&permissions=469888118",)
         embed.set_thumbnail(url=ctx.bot.user.avatar_url)
         embed.add_field(name="Uptime", value=self.get_bot_uptime(), inline=False)
-        embed.add_field(name="Developer", value="Paws#0001", inline=True)
+        embed.add_field(name="Dev", value="Paws#0001", inline=True)
         embed.add_field(name="Library", value="discord.py", inline=True)
         embed.add_field(name="Commands loaded", value=len([x.name for x in self.bot.commands]), inline=True)
         embed.add_field(name="Servers", value=f"{len(ctx.bot.guilds)} (average: {avgmembers} users/server )", inline=True)
@@ -160,6 +161,45 @@ class Information:
                 else:
                     pass
         await pollmsg.edit(content=f"{ctx.message.author.mention} created a poll that will end after {time} seconds!\n**{question}**\n\nTime's up!\n👍 = {upvote_count-1}\n\n👎 = {downvote_count-1}")
+
+    @commands.command()
+    async def suggest(self, ctx, *, suggestion_txt: str):
+        """ Send a suggestion to my owner or just tell him shes doing a bad job """
+        webhook = DiscordWebhook(url=f'{self.config.suggwebhook}')
+        suggestion = suggestion_txt
+        if ctx.guild:
+            color = ctx.author.color
+            footer = f"Sent from {ctx.guild.name}"
+            guild_pic = ctx.guild.icon_url
+        else:
+            color = 0x254d16
+            footer = "Sent from DMs"
+            guild_pic = ""
+        if len(suggestion) > 1500:
+            await ctx.send(f"{ctx.author.mention} thats a bit too long for me to send. Shorten it and try again. (1500 character limit)")
+        else:
+            suggestionem = DiscordEmbed(description=f"{suggestion}", colour=color)
+            suggestionem.set_author(name=f"From {ctx.author}", icon_url=ctx.author.avatar_url)
+            suggestionem.set_footer(text=footer, icon_url=guild_pic)
+            try:
+                await ctx.send("Alright, i sent your suggestion!!")
+                webhook.add_embed(suggestionem)
+                webhook.execute()
+            except Exception as e:
+                return await ctx.send("uhm.. something went wrong, try again later..")
+
+    @commands.command()
+    async def args(self, ctx, *args):
+        """Returns the number of args"""
+        await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+
+    @commands.command()
+    async def compare(self, ctx, str1: str = None, str2: str = None):
+        """ Compare 2 strings, put in "" if you want multiple words per string """
+        if str1 == str2:
+            await ctx.send("The two strings are the same!")
+        else:
+            await ctx.send("The two strings are different...")
 
 
 def setup(bot):
